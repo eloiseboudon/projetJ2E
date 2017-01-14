59ddaf3960b3b9db1b0e739aa20c9be3a84b1d63
Eloise <Eloise@192.168.1.88>
2017-01-14 13:03:37 +0100

    commit 4

---
+++ src/java/fr/elfoa/ejb/CreneauxEJB.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.elfoa.ejb;
+
+import fr.elfoa.entities.Creneaux;
+import fr.elfoa.entities.Medecin;
+import fr.elfoa.entities.RendezVous;
+
+/**
+ *
+ * @author Eloise
+ */
+public class CreneauxEJB {
+    
+    
+    
+    
+}
